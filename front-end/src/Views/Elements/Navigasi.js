25e0ac0123121b851066f814ebe06880f2e9f26d
ivan kissling <ivangiovn@gmail.com>
2020-07-18 14:44:26 +0800

    Updating font family and colour

--- front-end/src/Views/Elements/Navigasi.js
+++ front-end/src/Views/Elements/Navigasi.js
@@ -4,7 +4,7 @@ import '../Styles/Navigasi.css';
 
 export default class Navigasi extends Component {
     render() {
-        const text_selected = { color: "#d8345f" };
+        const text_selected = { color: "#75daad" };
         const text_color = { color: "black" };
         const nav_colour = { background: "#eeeeee" };
         const nav_fixed = { position: "fixed" };
@@ -12,9 +12,9 @@ export default class Navigasi extends Component {
         return (
             <Navbar className="justify-content-around shadow p-3 mb-5 bg-white rounded" style={ nav_colour }>
                 <Nav style={{fontWeight: "bold"}} >
-                    <Nav.Link style={text_selected} href="#home">EXPLORE</Nav.Link>
-                    <Nav.Link style={text_color} href="#features">TECHNOLOGY</Nav.Link>
-                    <Nav.Link style={text_color} href="#pricing">TUTORIAL</Nav.Link>
+                    <Nav.Link style={text_selected} href="#home">Explore</Nav.Link>
+                    <Nav.Link style={text_color} href="#features">Technology</Nav.Link>
+                    <Nav.Link style={text_color} href="#pricing">Tutorial</Nav.Link>
                     <Nav.Link style={text_color} href="#pricing">WhoAreWe??</Nav.Link>
                 </Nav>
             </Navbar>
