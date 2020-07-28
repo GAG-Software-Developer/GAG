71e0637dc25c1154f26685d3f72cd5c8ea8ed035
ivan kissling <ivangiovn@gmail.com>
2020-07-28 12:55:54 +0800

    Trying express in cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,45 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
+admin.initializeApp(functions.config().firebase);
+
+let db = admin.firestore();
+
+
+app.get('/', (req, res) => {
+    return res.status(200).send('Hello World');
+});
+
+exports.widgets = functions.https.onRequest(app);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
     functions.logger.info("Hello logs!", { structuredData: true });
     const nama = request.body.nama;
-    response.send("Hello nama saya " + nama);
+    const email = request.body.email;
+    const sex = request.body.sex;
+    
+     //init users
+    let usersRef = db.collection('users');
+    
+    //Isi row data
+    
+    usersRef.add(
+        {
+            nama: nama,
+            email: email,
+            telpon: '081231466200'
+        }, {
+            nama: "nama",
+            email: "email",
+            telpon: '081231466200'
+        }
+
+    );
+    ((addNewDoc) ?  response.send("sukses") :  response.send("failed"));
 }); 
