40e77e02f77a6c5f9896827ec9e6d50e10c85b31
Giovanni Tjahjono <ivangiovn@gmail.com>
2020-08-09 13:35:19 +0800

    Updating database schema

---
+++ GAG-Website/storage/framework/views/fd7adf37c1190762648963003dabae6d5641aa9c.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="<?php echo e(app()->getLocale()); ?>">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Passion+One:wght@700&display=swap" rel="stylesheet">
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+        <title>GAG - Explore Now</title>
+    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app"></div>
+    <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+</body>
+
+</html><?php /**PATH /Users/giovannitjahjono/Documents/Project/GAG/GAG-Website/resources/views/app.blade.php ENDPATH**/ ?>
